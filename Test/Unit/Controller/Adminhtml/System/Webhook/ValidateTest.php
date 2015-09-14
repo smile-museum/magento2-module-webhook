946f1d9252f73f95caf6fe6d129531a6e828541d
Bill Curtis <wcurtis@sweettoothhq.com>
2015-09-13 21:30:31 -0400

    Add stubbed tests - don't worry they're coming!

---
+++ Test/Unit/Controller/Adminhtml/System/Webhook/ValidateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace SweetTooth\Webhook\Test\Unit\Controller\Adminhtml\System\Webhook;
+
+/**
+ * TODO: Write tests
+ *
+ * WHAT!? NO TESTS!? BLASPHEMY!!
+ * 
+ * Yo, chill. We're all for writing tests but while this module is in
+ * beta (as is Magento 2 itself) TDD can be counter productive while many
+ * are learning the Magento 2 framework itself. These tests will come. All in
+ * good time :)
+ */
+class ValidateTest extends \PHPUnit_Framework_TestCase
+{
+}
---
+++ Test/Unit/Model/WebhookTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SweetTooth\Webhook\Test\Unit\Model;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
+/**
+ * TODO: Write tests
+ *
+ * WHAT!? NO TESTS!? BLASPHEMY!!
+ * 
+ * Yo, chill. We're all for writing tests but while this module is in
+ * beta (as is Magento 2 itself) TDD can be counter productive while many
+ * are learning the Magento 2 framework itself. These tests will come. All in
+ * good time :)
+ */
+class WebhookTest extends \PHPUnit_Framework_TestCase
+{
+}
